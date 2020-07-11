1c197e7c31d130937bfe30a275038d7797f9cd68
federicogag <federico.gagliazzi@gmail.com>
2020-07-11 10:59:22 +0200

    Added createDirs method to create log folder

--- TCGDatabase/src/it/unibs/db/tcg/model/util/LogWriter.java
+++ TCGDatabase/src/it/unibs/db/tcg/model/util/LogWriter.java
@@ -1,23 +1,30 @@
 package it.unibs.db.tcg.model.util;
 
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-
 public class LogWriter {
 	private static DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
 
 	public static void write(String toWrite) {
+		createDirs();
 		Date date = new Date();
-		try(BufferedWriter bw = new BufferedWriter(new FileWriter("log" + System.getProperty("file.separator") + "log.txt", true))) {
-			bw.write("[" + dateFormat.format(date) + "] "+ toWrite + "\n");
-		} 
-		catch (IOException e) {  
+		try (BufferedWriter bw = new BufferedWriter(
+				new FileWriter("log" + System.getProperty("file.separator") + "log.txt", true))) {
+			bw.write("[" + dateFormat.format(date) + "] " + toWrite + "\n");
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+
+	private static void createDirs() {
+		File file = new File("log");
+		file.mkdirs();
+		assert file.isDirectory() : "Errore nella creazione della directory" + file.getAbsolutePath();
 	}
 }
